38fd67a90ced249f0c86a34ea03d93efb4050944
zubeir <zubwd1@gmail.com>
2024-10-04 07:49:35 +0100

    correct script bd

--- src/assets/script.sql
+++ src/assets/script.sql
@@ -3,7 +3,7 @@ USE recipe_management;
 
 CREATE TABLE recipes (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    title VARCHAR(10) NOT NULL,
+    title VARCHAR(20) NOT NULL,
     type VARCHAR(100) NOT NULL,
     ingredients TEXT NOT NULL
 );
